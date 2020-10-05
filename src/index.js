a47d28c58570e2be30e714cd5bcaae2626f4a26a
Daniil-Mir <MironovDanill.S.19@gmail.com>
2020-10-05 17:53:33 +0500

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        if (str[i] === bracketsConfig[j][0]) { // Если скобка открывающая
+          if (str[i] === bracketsConfig[j][1] && stack.length !== 0 && stack[stack.length - 1] === bracketsConfig[j][0]) { // Если она такая же как закрывающая и в стеке последняя скобка такая же, то найдена закрывающая скобка 
+            stack.pop();
+          } else {
+            stack.push(str[i]);  // Найдена открывающая скобка
+          }
+          
+        } else if (str[i] === bracketsConfig[j][1]) { // Если скобка закрывающая
+           if (stack[stack.length - 1] === bracketsConfig[j][0]) { // Если в стеке есть такая же открывающая скобка
+             stack.pop();
+           } else if (stack[stack.length - 1] !== bracketsConfig[j][0]) { // Если последняя скобка в стеке не такая же открывающая
+             return false;
+           }
+        }
+      }
+    }
+  
+    if (stack.length !== 0) {
+      return false;
+    }
+  
+    return true;
 }
